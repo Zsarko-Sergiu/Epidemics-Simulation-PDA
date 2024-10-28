9e9f39386b6a6bdd71b4e3bab3211e556724714c
Cheris <cheris@Cheris>
2024-10-28 13:31:13 +0200

    Configured matrix; started movement for serial version

--- project.c
+++ project.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define FILE_BUFFER_SIZE 256
 int MAX_X_COORD,MAX_Y_COORD,N;
-
-void initialize(char* total_sim_time,char* file_name,char* nr_threads);
-void print_people();
 
 typedef struct person
 {
     int id,x,y,init_status,movement,amp;
 }person;
+
+void initialize(char* total_sim_time,char* file_name,char* nr_threads);
+void print_people();
+void print_matrix();
+void start_simulation_serial(int sim_time,int n,int infection_counter[n],struct person people[n],int matr[][n]);
+void move_person(person p,int n,int matr[][n]);
+
 
 int main(int argc,char* argv[])
 {
@@ -20,6 +25,109 @@ int main(int argc,char* argv[])
         exit(1);
     }
     initialize(argv[1],argv[2],argv[3]);
+}
+
+void print_matrix(int n,int matr[][n])
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",matr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void init_matrix(int n,int matr[][n])
+{
+    for (int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            matr[i][j]=0;
+        }
+    }
+}
+
+void move_person(person p,int n,int matr[][n])
+{
+    int direction=p.movement;
+    //left
+    if(direction==3)
+    {
+        //y decrease, x stays same
+        int new_y=p.y-p.amp;
+        if(new_y<0)
+        {
+            new_y=abs(new_y);
+            p.movement=2;
+        }
+        matr[p.x][p.y]=0;
+        matr[p.x][new_y]=p.id;
+        p.y=new_y;
+    }
+    //right
+    if(direction==2)
+    {
+        //y decrease, x stays same
+        int new_y=p.y+p.amp;
+        if(new_y>=10)
+        {
+            new_y=n-(new_y-n);
+            p.movement=3;
+        }
+        matr[p.x][p.y]=0;
+        matr[p.x][new_y]=p.id;
+        p.y=new_y;
+    }
+    //down
+    if(direction==1)
+    {
+        //x decrease, y stays same
+        int new_x=p.x-p.amp;
+        if(new_x<0)
+        {
+            new_x=abs(new_x);
+            p.movement=0;
+        }
+        matr[p.x][p.y]=0;
+        printf("%d %d\n",new_x,p.id);
+        matr[new_x][p.y]=p.id;
+        p.x=new_x;
+    }
+    //up
+    if(direction==0)
+    {
+        //x decrease, y stays same
+        int new_x=p.x+p.amp;
+        if(new_x>=10)
+        {
+            new_x=n-(new_x-n);
+            p.movement=1;
+        }
+        matr[p.x][p.y]=0;
+        matr[new_x][p.y]=p.id;
+        p.x=new_x;
+    }
+}
+
+void start_simulation_serial(int sim_time,int n,int infection_counter[n],person people[n],int matr[][n])
+{
+    while(sim_time>0)
+    {
+        for(int i=0;i<n;i++)
+        {
+            person p=people[i];
+            move_person(p,n,matr);
+        }
+        printf("Simulation time %d\n",sim_time);
+        print_matrix(n,matr);
+        printf("\n");
+        sim_time--;
+
+    }
+    printf("finished simulation\n");
 }
 
 void initialize(char* total_sim_time,char* file_name,char* nr_threads)
@@ -38,6 +146,10 @@ void initialize(char* total_sim_time,char* file_name,char* nr_threads)
 
     struct person people[N];
     int cnt=0;
+    //N by N matrix ; values in the matrix will represent the id's of the people
+    int matr[N][N];
+
+    init_matrix(N,matr);
 
     fgets(line,FILE_BUFFER_SIZE,f);
     while(fgets(line,FILE_BUFFER_SIZE,f))
@@ -49,10 +161,19 @@ void initialize(char* total_sim_time,char* file_name,char* nr_threads)
             &people[cnt].init_status,
             &people[cnt].movement,
             &people[cnt].amp);
+        matr[people[cnt].x][people[cnt].y]=people[cnt].id;
         cnt++;
     }
+    print_matrix(N,matr);
     print_people(people,N);
+    int infection_counter[N];
+    for(int i=0;i<N;i++)
+    {
+        infection_counter[i]=0;
+    }
     
+    int total_sim_time_conv=atoi(total_sim_time);
+    start_simulation_serial(total_sim_time_conv,N,infection_counter,people,matr);
 }
 
 void print_people(person* people,int N)
